90af7e7e19d446a9fa4bbf6d3c3f5bf90870a51b
CaoHoangPhuc <do.not.divide.heaven.in.two@gmail.com>
2018-11-01 23:47:09 +0700

    Add files via upload

---
+++ Agent.py
@@ -0,0 +1,144 @@
+import numpy as np
+import random
+from collections import deque
+import plaidml.keras
+
+plaidml.keras.install_backend()
+
+import keras
+from keras.layers import LeakyReLU
+
+class Agent(object):
+
+	def __init__(self):
+		self.ACTION = 2
+		self.batch = 32
+		self.gamma = 0.99
+		self.MaxMem = 50000
+		self.learning_rate = 1e-6
+
+		self.model = self._Create()
+		self.Mem = deque()
+
+	def _load(self,x):
+		if x!=0:
+			self.model.load_weights("Agent" + str(x) +".h5")
+			print("Loaded model sucessfully from disk")
+
+	def _save(self,x):
+		self.model.save_weights("Agent" + str(x) +".h5")
+		print("Saved model successfully to disk")
+
+	def save_mem(self,obs,act,rwd,n_o,ter):
+		if len(self.Mem) > self.MaxMem: self.Mem.popleft()
+		self.Mem.append((obs,np.argmax(act),rwd,n_o,ter))
+
+	def act(self,obs):
+		act = np.argmax(self.model.predict(np.array([obs])))
+		if act :
+			return [0,1]
+		else:
+			return [1,0]
+
+	def replay(self):
+		mini_batch = random.sample(self.Mem,self.batch)
+		S  = np.array([d[0] for d in mini_batch])
+		S1 = np.array([d[3] for d in mini_batch])
+
+		temp = self.model.predict(S)
+		temp1 = self.model.predict(S1)
+
+		for i in range(0,self.batch):
+			if mini_batch[i][4]:
+				temp[i][mini_batch[i][1]] = mini_batch[i][2]
+			else:
+				temp[i][mini_batch[i][1]] = mini_batch[i][2] + \
+				self.gamma*np.max(temp1[i])
+
+		self.model.train_on_batch(S,temp)
+
+	def _Create(self):
+
+		model = keras.models.Sequential()
+
+		RAN = keras.initializers.RandomNormal(
+			mean=0.0, stddev=.01, seed=None)
+
+		ACT = 'linear'
+
+		model.add(keras.layers.Reshape((1,80,80),
+			input_shape=(1,80,80)))
+
+		model.add(keras.layers.Conv2D(32,(20,20),
+			data_format="channels_first",
+			strides = 4,
+			padding ="same",
+			activation=ACT,
+			use_bias=True,
+			kernel_initializer=RAN,
+			bias_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))
+
+		model.add(keras.layers.Conv2D(64,(5,5),
+			data_format="channels_first",
+			strides = 5,
+			padding ="same",
+			activation=ACT,
+			use_bias=True,
+			kernel_initializer=RAN,
+			bias_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))
+
+		model.add(keras.layers.Conv2D(128,(3,3),
+			data_format="channels_first",
+			strides = 2,
+			padding ="same",
+			activation=ACT,
+			use_bias=True,
+			kernel_initializer=RAN,
+			bias_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))	
+
+		model.add(keras.layers.Conv2D(256,(2,2),
+			data_format="channels_first",
+			strides = 2,
+			padding ="same",
+			activation=ACT,
+			use_bias=True,
+			kernel_initializer=RAN,
+			bias_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))	
+		
+		model.add(keras.layers.Flatten())
+
+		model.add(keras.layers.Dense(256, 
+			activation=ACT,
+			use_bias=True,
+			bias_initializer=RAN,
+			kernel_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))
+
+		model.add(keras.layers.Dense(256, 
+			activation=ACT,
+			use_bias=True,
+			bias_initializer=RAN,
+			kernel_initializer=RAN))
+
+		model.add(LeakyReLU(alpha=.001))
+
+		model.add(keras.layers.Dense(self.ACTION,
+			activation='linear',
+			use_bias=True,
+			bias_initializer=RAN,
+			kernel_initializer=RAN))
+
+		opt = keras.optimizers.Adam(lr = self.learning_rate)
+		model.compile(optimizer=opt,loss='mse')
+
+		model.summary()
+		return model
---
+++ Bird.py
@@ -0,0 +1,76 @@
+import Agent
+import cv2
+import sys
+sys.path.append("game/")
+import random
+import wrapped_flappy_bird as game
+import numpy as np
+
+Bird = Agent.Agent()
+
+OBSERVE = 1000.
+EXPLORE = 1000000.
+INIT_EP = 0.5
+FINL_EP = 0.0001
+SAVE_SL = 1
+LOAD_SL = 0
+
+def preprocess(image):
+    x_t = cv2.cvtColor(cv2.resize(image, (80, 80)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+    return np.array([x_t])
+
+def random_act(obs,x):
+    if random.random() < x:
+        if random.random() < 0.1:
+            print("random_act ")
+            return [0,1]
+        else:
+            return [1,0]
+    else:
+        return Bird.act(obs)
+
+
+def play_flappy():
+
+    TIME_S  = 1     
+    EPSILON = INIT_EP
+
+    Bird._load(LOAD_SL)
+
+    F_obs, reward, terminal = flappy_game.frame_step([1,0])
+    F_obs = preprocess(F_obs)
+    Bird.act(F_obs)
+
+    while 1:        
+
+        obs = F_obs
+        terminal = 0
+
+        while terminal == 0:
+            
+            action = random_act(obs,EPSILON)
+            ne_obs, reward, terminal = flappy_game.frame_step(action)
+            ne_obs = preprocess(ne_obs)
+
+            Bird.save_mem(obs,action,reward,ne_obs,terminal)          
+            
+            if (TIME_S > OBSERVE) and (EPSILON > FINL_EP):
+                EPSILON -= (INIT_EP - FINL_EP)/EXPLORE
+            
+            if (TIME_S % 100 == 0):
+            	print(TIME_S,reward,np.argmax(action),round(EPSILON,5),round(
+                np.amax(Bird.model.predict(np.array([obs]))[0]),5))
+            
+            if (TIME_S >= OBSERVE):
+            	Bird.replay()
+
+            if TIME_S %10000 == 0:
+                Bird._save(SAVE_SL)
+
+            TIME_S +=1
+            obs = ne_obs
+
+flappy_game = game.GameState()
+
+play_flappy()
