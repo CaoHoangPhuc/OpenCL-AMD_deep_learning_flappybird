3eedb3a0098742396abebc9054afdb9835fb94cb
PhucC <do.not.divide.heaven.in.two@gmail.com>
2023-04-25 04:12:13 +0700

    Add new more play game

--- Gym.py
+++ Gym.py
@@ -18,16 +18,16 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = []
-        self.gamma = 0.99  # discount rate
-        self.epsilon = 0.1  # exploration rate
-        self.epsilon_min = 0.1
-        self.epsilon_decay = 0.998
+        self.gamma = 0.98  # discount rate
+        self.epsilon = 1.0 # exploration rate
+        self.epsilon_min = 0.0001
+        self.epsilon_decay = 0.999
         self.learning_rate = 1e-4
         self.model = self._build_model()
 
     def _build_model(self):
         model = Sequential()
-        model.add(keras.layers.Reshape((1,4), input_shape=(1,4)))
+        model.add(keras.layers.Reshape((1,self.state_size), input_shape=(1,self.state_size)))
         model.add(Dense(4*4, activation=LeakyReLU(alpha=.001)))
         model.add(Dense(4*4*4, activation=LeakyReLU(alpha=.001)))
         model.add(Dense(4*4*4*4, activation=LeakyReLU(alpha=.001)))
@@ -58,7 +58,7 @@ class DQNAgent:
         targets = self.model.predict(states)
         q_values_next = self.model.predict(next_states)
 
-        targets[np.arange(batch_size), 0, actions] = rewards + self.gamma * np.max(q_values_next.reshape(batch_size,2), axis = 1) * (1 - dones)
+        targets[np.arange(batch_size), 0, actions] = rewards + self.gamma * np.max(q_values_next.reshape(batch_size, action_size), axis = 1) * (1 - dones)
 
         self.model.train_on_batch(states, targets)
 
@@ -72,29 +72,33 @@ class DQNAgent:
     def save(self, name):
         self.model.save_weights(name)
 
+def dtlaction(action):
+    return [(action-2)]
 
 if __name__ == "__main__":
     # initialize gym environment and the agent
-    env = gym.make("CartPole-v1", render_mode="human")
+    # enviroment available in here https://www.gymlibrary.dev/environments/classic_control/
+    game_name = 'Pendulum-v1'
+    env = gym.make(game_name, render_mode="human")
     state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
+    action_size = 5 #env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    agent.load("cartpole-dqn.h5")
+    # agent.load(game_name + "_dqn.h5")
     # train the agent
-    done = False
-    batch_size = 64
+    time_step = 500
+    batch_size = 32
     for e in range(500):
+        rate = 0
         state = env.reset()  # Reset the environment and get the initial state
-        env.render() # uncomment to render the game
-        state = np.reshape(state[0], [1, 4])  # Reshape the state to have 4 features
-        for time in range(1000):
-            # env.render() # uncomment to render the game
+        state = np.reshape(state[0], [1, state_size])  # Reshape the state
+        for time in range(time_step):
             action, x = agent.act(state)
-            next_state, reward, done, _, _ = env.step(action)
-            reward = reward if not done else -10
+            next_state, reward, done, _, _ = env.step(dtlaction(action))
+            # reward = reward if not done else -20
             next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
             state = next_state
+            if time == (time_step - 1): done = True
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}, sc: {}"
                       .format(e, 500, time, agent.epsilon, x ))
@@ -103,5 +107,5 @@ if __name__ == "__main__":
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
         if e % 50 == 0:
-            agent.save("cartpole-dqn.h5")
+            agent.save(game_name + "_dqn.h5")
             print("saved")
